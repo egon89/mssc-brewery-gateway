bdc96c406d185fe789ae2c62e0db244937d4dd2d
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-05-26 22:38:55 -0300

    feat: example route config and profile

---
+++ src/main/java/com/egon89/config/ExampleRoute.java
@@ -0,0 +1,23 @@
+package com.egon89.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("example")
+@Configuration
+public class ExampleRoute {
+
+  @Bean
+  public RouteLocator exampleRouteConfig(RouteLocatorBuilder builder) {
+    System.out.println("example route config init");
+    return builder.routes()
+        .route(p -> p
+            .path("/get")
+            .filters(f -> f.addRequestHeader("x-custom-header", "hello-world"))
+            .uri("http://httpbin.org:80"))
+        .build();
+  }
+}
