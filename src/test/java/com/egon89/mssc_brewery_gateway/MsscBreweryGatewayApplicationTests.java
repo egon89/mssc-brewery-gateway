7c3b414494be51cef93729319fac83ed71ffd79c
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-05-26 21:07:09 -0300

    feat: project init

---
+++ src/test/java/com/egon89/mssc_brewery_gateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egon89.mssc_brewery_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
